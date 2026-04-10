8e40a199e8c9a4f55041ae57af0cf876ffdae3bc
Khoa Tran <108267692+khoatran1995@users.noreply.github.com>
2026-04-10 17:57:32 +0800

    Add copyright and license to run_discovery.py
    Add copyright notice and license information to the file.

--- run_discovery.py
+++ run_discovery.py
@@ -1,3 +1,11 @@
+# Copyright 2026 Khoa Tu Tran
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
 import pandas as pd
 from Bio import SeqIO
 from platygeno import PlatyGenoEngine
